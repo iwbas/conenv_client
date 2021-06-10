c1aebf807a31da83103e26a2b07836fac4c2f630
iwbas <brexceed@gmail.com>
2021-06-10 18:27:37 +0300

    ra added; ra-provider edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Admin, ListGuesser, Resource } from "react-admin";
+// import simpleRestProvider from "ra-data-simple-rest";
+import customDataProvider from "./providers/dataProvider";
 
-function App() {
-  return (
-    <div className="App">
-      HEYALL
-    </div>
-  );
-}
+const dataProvider = customDataProvider("http://localhost:8080/api");
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+    <Resource name="roles" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
